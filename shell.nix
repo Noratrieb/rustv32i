f88ec3c7eb546609186ad17222da31d34218f66f
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2025-03-09 17:50:08 +0100

    test branch

--- shell.nix
+++ shell.nix
@@ -1,5 +1,4 @@
 { pkgs ? import <nixpkgs> { } }: pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ cmake ninja ];
   packages = with pkgs; [
     llvmPackages_18.clang-unwrapped
     llvmPackages_18.lld
